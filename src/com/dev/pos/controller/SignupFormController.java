be668399c09475fb25b8d747c818d9445a633b90
rashmika523 <anjurashmika188@gmail.com>
2024-08-05 15:27:36 +0530

    Create user

--- src/com/dev/pos/controller/SignupFormController.java
+++ src/com/dev/pos/controller/SignupFormController.java
@@ -1,14 +1,19 @@
 package com.dev.pos.controller;
 
+import com.dev.pos.db.DBConnection;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class SignupFormController {
     public AnchorPane context;
@@ -23,6 +28,23 @@ public class SignupFormController {
     }
 
     public void btnSignupOnAction(ActionEvent actionEvent) {
+
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            String sql = "INSERT INTO user VALUES (?,?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1,txtEmail.getText());
+            preparedStatement.setString(2,txtPassword.getText());
+            int count = preparedStatement.executeUpdate();
+            if(count>0){
+                new Alert(Alert.AlertType.INFORMATION,"User has been Saved...!").show();
+            }else {
+                new Alert(Alert.AlertType.INFORMATION,"Something went wrong, Try again...!").show();
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            new Alert(Alert.AlertType.ERROR,e.getMessage().toString()).show();
+        }
+
 
     }
 
