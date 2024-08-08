23f8b15b15ace13bb33892fcdad23ca401fda704
rashmika523 <anjurashmika188@gmail.com>
2024-08-08 10:01:23 +0530

    implemented product crud in ProductDaoImpl

--- src/com/dev/pos/dao/custom/ProductDao.java
+++ src/com/dev/pos/dao/custom/ProductDao.java
@@ -2,22 +2,23 @@ package com.dev.pos.dao.custom;
 
 import com.dev.pos.Entity.Product;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface ProductDao {
 
-    public boolean saveProduct(Product product);
+    public boolean saveProduct(Product product) throws SQLException, ClassNotFoundException;
 
-    public boolean updateProduct(Product product);
+    public boolean updateProduct(Product product) throws SQLException, ClassNotFoundException;
 
-    public boolean deleteProduct(int code);
+    public boolean deleteProduct(int code) throws SQLException, ClassNotFoundException;
 
-    public Product findProduct(int code);
+    public Product findProduct(int code) throws SQLException, ClassNotFoundException;
 
-    public List<Product> findAllProduct();
+    public List<Product> findAllProduct() throws SQLException, ClassNotFoundException;
 
-    public List<Product> searchProduct(String value);
+    public List<Product> searchProduct(String value) throws SQLException, ClassNotFoundException;
 
-    public  int getLastProductId();
+    public  int getLastProductId() throws SQLException, ClassNotFoundException;
 
 }
