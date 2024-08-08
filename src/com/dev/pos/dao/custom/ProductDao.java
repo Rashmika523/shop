3c569fee304fd23413c53b5cdf620cde310c9a39
rashmika523 <anjurashmika188@gmail.com>
2024-08-08 09:33:31 +0530

    wrote product crud in ProductDao

--- src/com/dev/pos/dao/custom/ProductDao.java
+++ src/com/dev/pos/dao/custom/ProductDao.java
@@ -1,4 +1,23 @@
 package com.dev.pos.dao.custom;
 
+import com.dev.pos.Entity.Product;
+
+import java.util.List;
+
 public interface ProductDao {
+
+    public boolean saveProduct(Product product);
+
+    public boolean updateProduct(Product product);
+
+    public boolean deleteProduct(int code);
+
+    public Product findProduct(int code);
+
+    public List<Product> findAllProduct();
+
+    public List<Product> searchProduct(int code);
+
+    public  int getLastProductId();
+
 }
