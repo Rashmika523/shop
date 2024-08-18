2b0585e930fa983e61ef592d7a62e55c0e2f01a9
rashmika523 <anjurashmika188@gmail.com>
2024-08-18 11:01:53 +0530

    created user bo interface with methods

--- src/com/dev/pos/bo/custom/UserBo.java
+++ src/com/dev/pos/bo/custom/UserBo.java
@@ -1,2 +1,13 @@
-package com.dev.pos.bo.custom;public interface UserBo {
+package com.dev.pos.bo.custom;
+
+import com.dev.pos.dto.UserDTO;
+
+import java.sql.SQLException;
+
+public interface UserBo {
+
+    public boolean saveUser(UserDTO  dto) throws SQLException, ClassNotFoundException;
+
+    public UserDTO findUser(String email) throws SQLException, ClassNotFoundException;
+
 }
