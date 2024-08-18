6db1bbde33abe7dea5ec15bbeae2bff97cb04d90
rashmika523 <anjurashmika188@gmail.com>
2024-08-18 10:58:14 +0530

    created product bo interface with methods

--- src/com/dev/pos/bo/custom/ProductBo.java
+++ src/com/dev/pos/bo/custom/ProductBo.java
@@ -1,4 +1,26 @@
 package com.dev.pos.bo.custom;
 
+import com.dev.pos.Entity.Product;
+import com.dev.pos.dto.ProductDTO;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface ProductBo {
+
+
+    public boolean saveProduct(ProductDTO  dto) throws SQLException, ClassNotFoundException;
+
+    public boolean updateProduct(ProductDTO dto) throws SQLException, ClassNotFoundException;
+
+    public boolean deleteProduct(int code) throws SQLException, ClassNotFoundException;
+
+    public ProductDTO findProduct(int code) throws SQLException, ClassNotFoundException;
+
+    public List<ProductDTO> findAllProducts() throws SQLException, ClassNotFoundException;
+
+    public List<ProductDTO> searchByDescription(String value) throws SQLException, ClassNotFoundException;
+
+    public  int getLastProductId() throws SQLException, ClassNotFoundException;
+
 }
