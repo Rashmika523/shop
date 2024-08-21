495645c4a9aa5817d6cecd966c785ee61942db23
rashmika523 <anjurashmika188@gmail.com>
2024-08-21 11:22:31 +0530

    Created Batch Entity class

--- src/com/dev/pos/controller/ProductMainFormController.java
+++ src/com/dev/pos/controller/ProductMainFormController.java
@@ -132,7 +132,7 @@ public class ProductMainFormController {
     public void btnAddNewBatchOnAction(ActionEvent actionEvent) throws IOException {
 
 
-        if(txtProductCode.getText().trim().length()>0){
+        if(txtSelectedProductCode.getText().trim().length()>0){
 
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/NewBatchForm.fxml"));
             Parent parent = fxmlLoader.load();
