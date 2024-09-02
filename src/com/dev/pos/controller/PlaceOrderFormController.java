59a1bb0be16c9c7bd9afba4ad08d4290383274ea
rashmika523 <anjurashmika188@gmail.com>
2024-09-02 10:42:40 +0530

    created isExist and clearFiled methods

--- src/com/dev/pos/controller/PlaceOrderFormController.java
+++ src/com/dev/pos/controller/PlaceOrderFormController.java
@@ -42,20 +42,20 @@ public class PlaceOrderFormController {
     public TextField txtQty;
 
     public TableView<CartTm> tblOrder;
-    public TableColumn<CartTm,String> colBarcode;
-    public TableColumn<CartTm,String> colDescription;
-    public TableColumn<CartTm,Double> colSellingPrice;
-    public TableColumn<CartTm,Double> colDiscount;
-    public TableColumn<CartTm,Double> colShowPrice;
-    public TableColumn<CartTm,Integer> colQty;
-    public TableColumn<CartTm,Double> colTotal;
-    public TableColumn<CartTm,Button> colDelete;
+    public TableColumn<CartTm, String> colBarcode;
+    public TableColumn<CartTm, String> colDescription;
+    public TableColumn<CartTm, Double> colSellingPrice;
+    public TableColumn<CartTm, Double> colDiscount;
+    public TableColumn<CartTm, Double> colShowPrice;
+    public TableColumn<CartTm, Integer> colQty;
+    public TableColumn<CartTm, Double> colTotal;
+    public TableColumn<CartTm, Button> colDelete;
     public Label lblTotal;
 
     CustomerBo customerBo = BoFactory.getInstance().getBo(BoType.CUSTOMER);
     BatchBo batchBo = BoFactory.getInstance().getBo(BoType.BATCH);
 
-    public void initialize(){
+    public void initialize() {
         colBarcode.setCellValueFactory(new PropertyValueFactory<>("barcode"));
         colDescription.setCellValueFactory(new PropertyValueFactory<>("description"));
         colSellingPrice.setCellValueFactory(new PropertyValueFactory<>("sellingPrice"));
@@ -70,27 +70,27 @@ public class PlaceOrderFormController {
     }
 
     public void btnBackToHome(ActionEvent actionEvent) throws IOException {
-        setUI("DashboardForm",false);
+        setUI("DashboardForm", false);
     }
 
     public void btnAddNewCustomer(ActionEvent actionEvent) throws IOException {
-        setUI("CustomerForm",true);
+        setUI("CustomerForm", true);
     }
 
     public void btnAddNewProduct(ActionEvent actionEvent) throws IOException {
-        setUI("ProductMainForm",true);
+        setUI("ProductMainForm", true);
     }
 
-    private void setUI(String location,boolean state) throws IOException {
+    private void setUI(String location, boolean state) throws IOException {
         Stage stage = null;
-        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("../view/"+location+".fxml")));
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("../view/" + location + ".fxml")));
 
-        if(state){
+        if (state) {
             stage = new Stage();
             stage.setScene(scene);
             stage.show();
             stage.centerOnScreen();
-        }else {
+        } else {
             stage = (Stage) context.getScene().getWindow();
             stage.setScene(scene);
             stage.centerOnScreen();
@@ -100,24 +100,24 @@ public class PlaceOrderFormController {
 
     public void searchCustomerOnAction(ActionEvent actionEvent) {
 
-        try{
-            CustomerDTO dto =customerBo.findCustomer(txtEmail.getText().trim());
-            if(dto!=null){
+        try {
+            CustomerDTO dto = customerBo.findCustomer(txtEmail.getText().trim());
+            if (dto != null) {
                 txtContact.setText(dto.getContact());
                 txtName.setText(dto.getName());
                 txtSalary.setText(String.valueOf(dto.getSalary()));
                 fetchLoyalityCardData(txtEmail.getText().trim());
                 txtBarcode.requestFocus();
-            }else {
-                new Alert(Alert.AlertType.INFORMATION,"Customer Not Found...!").show();
+            } else {
+                new Alert(Alert.AlertType.INFORMATION, "Customer Not Found...!").show();
             }
-        }catch (SQLException | ClassNotFoundException e){
+        } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
 
     }
 
-    private void fetchLoyalityCardData(String email){
+    private void fetchLoyalityCardData(String email) {
 
         hyprLoyalityDetails.setVisible(true);
         hyprLoyalityDetails.setText("+ New Loyalty");
@@ -127,7 +127,7 @@ public class PlaceOrderFormController {
         try {
             ProductDetailsJoinDto joinDto = batchBo.findProductJoinDetail(txtBarcode.getText().trim());
 
-            if(joinDto!=null){
+            if (joinDto != null) {
                 txtDescription.setText(joinDto.getDescription());
                 txtDescount.setText(String.valueOf(0));
                 txtSellingPrice.setText(String.valueOf(joinDto.getBatchDTO().getSellingPrice()));
@@ -135,11 +135,11 @@ public class PlaceOrderFormController {
                 txtBuyingPrice.setText(String.valueOf(joinDto.getBatchDTO().getBuyingPrice()));
                 txtQtyOnHand.setText(String.valueOf(joinDto.getBatchDTO().getQtyOnHand()));
                 txtQty.requestFocus();
-            }else {
-                new Alert(Alert.AlertType.INFORMATION,"Product Not Found...!").show();
+            } else {
+                new Alert(Alert.AlertType.INFORMATION, "Product Not Found...!").show();
             }
 
-        }catch (SQLException | ClassNotFoundException e){
+        } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
@@ -148,30 +148,66 @@ public class PlaceOrderFormController {
 
     public void addToCartOnAction(ActionEvent actionEvent) {
 
-        int qty =Integer.parseInt(txtQty.getText());
+        int qty = Integer.parseInt(txtQty.getText());
         double sellingPrice = Double.parseDouble(txtSellingPrice.getText());
         double total = qty * sellingPrice;
-        Button button = new Button("Remove");
 
-        if(Integer.parseInt(txtQtyOnHand.getText())>qty){
+        CartTm selectedItem = isExist(txtBarcode.getText().trim());
 
-            CartTm tm = new CartTm(
-                    txtBarcode.getText(),
-                    txtDescription.getText(),
-                    sellingPrice,Double.parseDouble(txtDescount.getText()),
-                    Double.parseDouble(txtShowPrice.getText()),
-                    qty,
-                    total,
-                    button
-            );
+        if(selectedItem==null){
 
-            oblist.add(tm);
+            Button button = new Button("Remove");
+            if (Integer.parseInt(txtQtyOnHand.getText()) > qty) {
 
-            tblOrder.setItems(oblist);
+                CartTm tm = new CartTm(
+                        txtBarcode.getText(),
+                        txtDescription.getText(),
+                        sellingPrice, Double.parseDouble(txtDescount.getText()),
+                        Double.parseDouble(txtShowPrice.getText()),
+                        qty,
+                        total,
+                        button
+                );
 
+                oblist.add(tm);
+
+                tblOrder.setItems(oblist);
+                clearFileds();
+                txtBarcode.requestFocus();
+
+            } else {
+                new Alert(Alert.AlertType.ERROR, "Please Add Valid Quantity").show();
+            }
         }else {
-            new Alert(Alert.AlertType.ERROR,"Please Add Valid Quantity").show();
+            selectedItem.setQty(qty+selectedItem.getQty());
+            selectedItem.setTotal(total+selectedItem.getTotal());
+            tblOrder.refresh();
+            clearFileds();
+            txtBarcode.requestFocus();
         }
 
+
+
+    }
+
+    private CartTm isExist(String code) {
+
+        for (CartTm tm : oblist) {
+            if (tm.getBarcode().equals(code)) {
+                return tm;
+            }
+        }
+
+        return null;
+    }
+
+    private void clearFileds(){
+        txtBarcode.clear();
+        txtQty.clear();
+        txtQtyOnHand.clear();
+        txtBuyingPrice.clear();
+        txtDescount.clear();
+        txtShowPrice.clear();
+        txtSellingPrice.clear();
     }
 }
