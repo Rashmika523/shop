a8fc39a702bfc36137b3716cee2eae77afaf3444
rashmika523 <anjurashmika188@gmail.com>
2024-09-09 01:01:14 +0530

    created ItemDetailDaoImpl save

--- src/com/dev/pos/dao/impl/ItemDetailDaoImpl.java
+++ src/com/dev/pos/dao/impl/ItemDetailDaoImpl.java
@@ -1,6 +1,7 @@
 package com.dev.pos.dao.impl;
 
 import com.dev.pos.Entity.ItemDetail;
+import com.dev.pos.dao.CrudUtil;
 import com.dev.pos.dao.custom.ItemDetailDao;
 
 import java.sql.SQLException;
@@ -9,7 +10,15 @@ import java.util.List;
 public class ItemDetailDaoImpl implements ItemDetailDao {
     @Override
     public boolean save(ItemDetail itemDetail) throws SQLException, ClassNotFoundException {
-        return false;
+
+        String sql = "INSERT INTO order_detail_has_prodct_details VALUES (?,?,?,?,?)";
+        return  CrudUtil.execute(sql,
+                itemDetail.getOrderDetailCode(),
+                itemDetail.getOrder(),
+                itemDetail.getQty(),
+                itemDetail.getDiscount(),
+                itemDetail.getAmount()
+        );
     }
 
     @Override
